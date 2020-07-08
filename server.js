ed7af01cb07f8db417fe251d61f6ecdb0dda0de2
Tanner Nielsen <tan-ali5@outlook.com>
2020-07-07 23:10:20 -0700

    init commit

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+
+const PORT = process.env.PORT || 8080;
+
+const app = express();
+
+app.use(express.static("public"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//Handlebars
+const exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// Importing routes 
+const routes = require("./controllers/controller.js");
+
+app.use(routes);
+
+app.listen(PORT, function() {
+  console.log("Server listening on: http://localhost:" + PORT);
+});
